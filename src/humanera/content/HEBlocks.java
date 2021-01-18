faced696c1d8310a246a26cb1afd0a0c0ee65083
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-01-18 18:20:23 +0900

    h

--- src/humanera/content/HEBlocks.java
+++ src/humanera/content/HEBlocks.java
@@ -32,5 +32,21 @@ public class HEBlocks implements ContentList {
             targetBlock = Blocks.pine;
             hoeTime = 1.5f * 60;
         }};
+        
+        formWork = new GenericSmelter("Formwork"){{
+            requirements(Category.crafting, with(Items.wood, 100, Items.stone, 50));
+            
+            craftEffect = Fx.smeltsmoke;
+            ouputItem = new Itemstack(HEItems.bronze, 1);
+            size = 2;
+            craftTime = 50f;
+            hasPower = false;
+            hasItems = true;
+            hasLiquids = false;
+            
+            flameColor = Color.valueOf("e25822");
+            
+            consumes.items(with(Items.copper, 3, HEItems.tin, 1));
+        });
     }
 }

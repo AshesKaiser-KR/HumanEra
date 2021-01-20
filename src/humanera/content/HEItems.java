32d1e80f83da7308eed804a65e45004e2ab20fe9
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-01-20 12:54:56 +0900

    h

--- src/humanera/content/HEItems.java
+++ src/humanera/content/HEItems.java
@@ -4,11 +4,12 @@ import mindustry.ctype.ContentList;
 import mindustry.type.Item;
 
 public class HEItems implements ContentList {
-    public static Item wood, stone, cassiterite;
+    public static Item wood, stone, cassiterite, tin;
 
     public void load(){
         wood = new Item("wood");
         stone = new Item("Stone");
+        tin = new Item("tin");
         cassiterite = new Item("Cassiterite");
     }
 }

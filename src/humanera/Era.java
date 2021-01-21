77ec7c794e737a3528edcaef73fa6d2830389639
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-01-21 17:25:38 +0900

    아니 이거 어떻게 하는데요

--- src/humanera/Era.java
+++ src/humanera/Era.java
@@ -1,9 +1,21 @@
 package humanera.content;
 
+import arc.graphics.Color;
+import mindustry.ctype.ContentType;
+import mindustry.ctype.UnlockableContent;
+
 //hhhhhhhh//
 
 public class Era extends UnlockableContent{
     public Era(String name){
         super(name);
     }
+    
+    public void active(){
+        
+    }
+    @Override
+    public ContentType getContentType(){
+        
+        return ContentType.typeid_UNUSED;
 }
